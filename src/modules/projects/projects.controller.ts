a071a5fd2c3b9e15996b6749d6e4d8e6e46e66b3
Bogdan Patratanu <bogdan.patratanu@gmail.com>
2026-01-24 17:53:41 +0200

    date filter

--- src/modules/projects/projects.controller.ts
+++ src/modules/projects/projects.controller.ts
@@ -161,7 +161,7 @@ export class ProjectsController extends BaseCrudController<Project> {
 
     const filters = {
       status: 'active',
-      endDate: Between(new Date(Date.now()), new Date(new Date().setDate(new Date().getDate() + 1))),
+      endDate: Between(new Date(Date.now()).toISOString(), new Date(new Date().setDate(new Date().getDate() + 1)).toISOString()),
     };
     console.log('filters', filters);
     const result = await this.service.findAllPaginated(filters, pageNumber, 1000, {submitDate: 'ASC'});
